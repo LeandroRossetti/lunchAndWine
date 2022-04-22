29c5d6d6f2cf3da6f2a3cc9ff0ac7419e88bf9f1
Leandro Rossetti <leandro.rossetti92@gmail.com>
2022-04-22 13:05:32 -0300

    correccion de proyecto

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 import { getDolar } from "./dolar.js";
 import { returnProducts } from "./menuListing.js";
-let carrito=[];
-let modalDiv=document.getElementById("modalDiv");
+
+let totalH6 = document.getElementById("totalH6")
+let modalDiv = document.getElementById("modalDiv");
 let total = 0;
 let restStorage = 0;
 let valueDolarDiv = document.getElementById("valueDolar");
-let iconoCarrito=document.getElementById("iconoCarrito");
+let iconoCarrito = document.getElementById("iconoCarrito");
+let columnDesc = document.getElementById("colDesc");
+let colPrice = document.getElementById("colPrice")
+
+
+
+
 async function dolarInNav() {
 
     let value = await getDolar()
@@ -68,35 +75,71 @@ document.addEventListener("DOMContentLoaded", function () {
 
         let btn = document.getElementById(btnId)
         btn.onclick = function () {
-            iconoCarrito
+
             if ((localStorage.getItem(iterator.id)) > 0) {
 
                 total += Number(iterator.price)
                 restStorage = localStorage.getItem(idProduct)
                 localStorage.removeItem(idProduct)
                 restStorage -= 1;
-                localStorage.setItem(idProduct, restStorage)
-              
-                modalDiv.innerHTML=`<p>Item: ${iterator.name} $ ${iterator.price} </p>\n`
-                carrito.push(iterator.name, iterator.price)
-                console.log(carrito)
-                console.log(modalDiv)
-                console.log(total)
-                console.log(restStorage)
-            }
-else{
-    return console.log('no hay stock papu');
-}
-        }
-        //se guarda en local storage el stock y el id de los productos
-        /* let stockId= parseInt(  localStorage.getItem(iterator.getIdElement())) */
 
+                localStorage.setItem(idProduct, restStorage)
+                let carrito = [];
+                carrito.push({ nombre: iterator.name, precio: iterator.price })
+
+                let carritoInLocal = localStorage.getItem('carrito');
+
+
+                if (!carritoInLocal) {
+                    localStorage.setItem('carrito', JSON.stringify(carrito));
+                } else {
+
+                    let newCarrito = [];
+                    newCarrito.push(JSON.parse(carritoInLocal));
+                    for (let i = 0; i < carrito.length; i++) {
+                        const element = carrito[i];
+                        newCarrito.push(element)
+                    }
+                    console.log(newCarrito)
+                    localStorage.setItem('carrito', JSON.stringify(newCarrito));
+
+                }
+
+
+
+                if (carrito.length >= 1) {
+
+                    for (let i = 0; i < carrito.length; i++) {
+
+                        const element = carrito[i];
+                        let generatePDesc = document.createElement('p');
+                        let generatePPrice = document.createElement('p');
+                        generatePDesc.innerHTML = element.nombre;
+                        generatePPrice.innerHTML = `$${element.precio}`;
+
+                        columnDesc.appendChild(generatePDesc);
+                        colPrice.appendChild(generatePPrice);
+
+
+                    }
+                }
+                totalH6.innerHTML = '$' + total.toString();
+    
+            }
+            else {
+                return Swal.fire({
+                    title: 'Sin stock',
+                    text: `El producto ${iterator.name} se acabo.`,
+                    icon: 'error',
+                    confirmButtonText: 'Confirmar'
+                })
+            }
+        }
+    
 
 
 
     }
 });
-/* 
-let btn1=document.getElementById(`btnDegustacion_1`);
 
- */
+
